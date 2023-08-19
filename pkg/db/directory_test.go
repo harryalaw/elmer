79fff047a77ed4504dd32882e357bec919bf1aed
harryalaw <harryalaw@gmail.com>
2023-08-19 11:44:26 +0100

    Formatting

--- pkg/db/directory_test.go
+++ pkg/db/directory_test.go
@@ -1,35 +1,34 @@
 package db
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestUse(t *testing.T) {
-    path := "/test/dir/foo"
-    score := 10
-    lastAccessed := int64(1692435000)
+	path := "/test/dir/foo"
+	score := 10
+	lastAccessed := int64(1692435000)
 
-    d := Dir{
-        path: path,
-        score : score,
-        lastAccessed: lastAccessed,
-    }
+	d := Dir{
+		path:         path,
+		score:        score,
+		lastAccessed: lastAccessed,
+	}
 
-    output := d.Use();
+	output := d.Use()
 
-    if output != path {
-        t.Fatalf("Output path was: %s, expected: %s", output, path)
-    }
+	if output != path {
+		t.Fatalf("Output path was: %s, expected: %s", output, path)
+	}
 
-    if d.path != path {
-        t.Fatalf("Path has changed, was: %s, expected: %s", d.path, path)
-    }
-    if d.score != score + 1{
-        t.Fatalf("score was: %d, expected: %d", d.score, score + 1)
-    }
+	if d.path != path {
+		t.Fatalf("Path has changed, was: %s, expected: %s", d.path, path)
+	}
+	if d.score != score+1 {
+		t.Fatalf("score was: %d, expected: %d", d.score, score+1)
+	}
 
-    if d.lastAccessed <= lastAccessed {
-        t.Fatalf("lastAccessed was: %d, expected: %d", d.lastAccessed, lastAccessed)
-    }
+	if d.lastAccessed <= lastAccessed {
+		t.Fatalf("lastAccessed was: %d, expected: %d", d.lastAccessed, lastAccessed)
+	}
 }
-
